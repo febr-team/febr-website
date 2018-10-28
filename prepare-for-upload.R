7cef07136d3af10a31eaffc4f1cce13ddbfba4ea
MatheusFerreiraR <matheusramos@alunos.utfpr.edu.br>
2018-10-28 00:03:21 -0300

    Fiz algumas alterações, mas ainda nao está pronto. Como nao está pronto, deixei a parte do Windows comentada.

--- prepare-for-upload.R
+++ prepare-for-upload.R
@@ -1,3 +1,4 @@
+if(sys.info == "linux"){
 # Build site
 rmarkdown::render_site(encoding = 'UTF-8')
 
@@ -5,6 +6,7 @@ rmarkdown::render_site(encoding = 'UTF-8')
 folders <- c("carousel", "clients", "testimonials")
 folders <- glue::glue("rm -r public/img/{folders}")
 lapply(folders, system)
+
 
 # Remove large and unwanted files
 files <- c("photogrid.svg", "texture-bw.png", "utfpr.svg")
@@ -16,3 +18,42 @@ system("cd public && zip -r upload.zip *")
 
 # Open server portal
 browseURL("http://www.suporte.cpd.ufsm.br/newftp/")
+
+# }else if(sys.info = "Windows"){
+# 
+# ## Build site
+# rmarkdown::render_site(encoding = 'UTF-8')
+#   
+# ## Remove folders  
+# # folders <- c("carousel", "clients", "testimonials")
+# # folders <- glue::glue("rm -r /public/img/{folders}")
+# # lapply(folders, system)
+#  
+#   Esse modo funcionar no windowns
+#   cd public/img
+#   rm -r clients carousel testimonials 
+#   Porem, precisa ser automatizado como o de cima
+# ==============================================================
+# # Remove large and unwanted files
+#   # files <- c("photogrid.svg", "texture-bw.png", "utfpr.svg")
+#   # files <- glue::glue("rm public/img/{files}")
+#   # lapply(files, system)
+#   
+#   funciona no Windowns:
+#   cd public/img
+#   rm -r photogrid.svg texture-bw.png utfpr.svg
+#   Porem, precisa ser automatizado como o de cima  
+# ==============================================================
+#   Esse modo nao funciona
+#   #Compress files
+#
+#   system2("cd public && winrar a -afzip -r upload.zip")
+#
+#   funciona no Windowns:
+#   cd public && winrar a -afzip -r upload.zip
+# ==============================================================
+#  
+# # Open server portal
+# browseURL("http://www.suporte.cpd.ufsm.br/newftp/")
+#   
+# }
